5c916e00df2e68848207153cf8b62f427d2e9e9a
buzzzlight <pomodoro5292@gmail.com>
2022-10-14 14:34:42 +0900

    Update delete account

--- accounts/views.py
+++ accounts/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 
 # from django.contrib.auth.forms import UserCreationForm
 from .forms import CustomUserChangeForm, CustomUserCreationForm
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, update_session_auth_hash
 from django.contrib.auth.forms import AuthenticationForm, PasswordChangeForm
 from django.contrib.auth import login as auth_login
 from django.contrib.auth import logout as auth_logout
@@ -76,6 +76,7 @@ def password(request):
         form = PasswordChangeForm(request.user, request.POST)
         if form.is_valid():
             form.save()
+            update_session_auth_hash(request, form.user)
             return redirect('accounts:index')
     else:
         form = PasswordChangeForm(request.user)
@@ -83,3 +84,9 @@ def password(request):
         'form': form,
     }
     return render(request, 'accounts/password.html', context)
+
+@login_required
+def delete(request):
+    request.user.delete()
+    auth_logout(request)
+    return redirect('accounts:index')
