2a36b6ad31d1c1f66f82cec3b566bb0d44567c4c
buzzzlight <pomodoro5292@gmail.com>
2022-10-14 14:07:08 +0900

    Update password

--- accounts/views.py
+++ accounts/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render, redirect
 # from django.contrib.auth.forms import UserCreationForm
 from .forms import CustomUserChangeForm, CustomUserCreationForm
 from django.contrib.auth import get_user_model
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, PasswordChangeForm
 from django.contrib.auth import login as auth_login
 from django.contrib.auth import logout as auth_logout
 from django.contrib.auth.decorators import login_required
@@ -69,3 +69,17 @@ def update(request):
         "form": form,
     }
     return render(request, "accounts/update.html", context)
+
+@login_required
+def password(request):
+    if request.method == 'POST':
+        form = PasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('accounts:index')
+    else:
+        form = PasswordChangeForm(request.user)
+    context = {
+        'form': form,
+    }
+    return render(request, 'accounts/password.html', context)
